e7b3715dd1c5dbfec20ee86f1e682b1a6b1aae76
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-10-01 18:19:20 +0200

    url params

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,27 @@ app.get('/build', (c) => {
 app.post('/build', async (c) => {
   console.log('POST /build');
 
-  // get the key= parameter and check if it matches the secret (process.env.TYPST_SECRET)
+  // get the key parameter and check if it matches the secret (process.env.TYPST_SECRET)
   const key = c.req.query('key');
   if (key !== process.env.TYPST_SECRET) {
     console.log(key, process.env.TYPST_SECRET);
     return c.body('Invalid key', 401);
   }
+
+  // get the entry file name and check if it is a typst file
+  const entryfile = c.req.query('entryfile') || 'main.typ';
+  if (!entryfile.endsWith('.typ')) {
+    return c.body('Invalid entry filename', 400);
+  }
+
+  // get the output file name and check if it is a pdf file
+  const outputfile = c.req.query('outputfile') || 'main.pdf';
+  if (!outputfile.endsWith('.pdf')) {
+    return c.body('Invalid output filename', 400);
+  }
+
+  // get the root argument and check if it is a string
+  const rootArg = c.req.query('root') || '';
 
   // get the zip file
   const body = await c.req.parseBody();
@@ -40,11 +55,14 @@ app.post('/build', async (c) => {
   const projectPath = `${extractPath}/${zip.name.replace('.zip', '')}`;
 
   // typst compile path/to/source.typ path/to/output.pdf
-  const sourcePath = `${projectPath}/main.typ`;
-  const outputPath = `${projectPath}/main.pdf`;
+  const sourcePath = `${projectPath}/${entryfile}`;
+  const outputPath = `${projectPath}/${outputfile}`;
 
   // compile the typst project
-  await $`typst compile ${sourcePath} ${outputPath}`;
+  if (rootArg)
+    await $`typst compile ${sourcePath} ${outputPath} --root ${rootArg}`;
+  else
+    await $`typst compile ${sourcePath} ${outputPath}`;
 
   // read the compiled pdf and clear the temp folder
   const output = await Bun.file(outputPath).arrayBuffer();
@@ -54,7 +72,7 @@ app.post('/build', async (c) => {
   // return the compiled pdf
   return c.body(output, 200, {
     'Content-Type': 'application/pdf',
-    'Content-Disposition': 'attachment; filename="main.pdf"',
+    'Content-Disposition': `attachment; filename="${outputfile}"`
   });
 
 })
